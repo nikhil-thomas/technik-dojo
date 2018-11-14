bc09a08792fb7a24b68ac4b3dbdf4b0c06e7851e
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-11-14 20:28:50 +0530

    Add Golang unit tests exercises using Go reflection
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ learn-go-with-tests/11_reflection/walk/walk_test.go
@@ -0,0 +1,127 @@
+package walk
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct wuith non string fields",
+			struct {
+				Name string
+				Age  int
+			}{
+				"Chris",
+				33,
+			},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(s string) {
+				got = append(got, s)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, expected %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+
+	// maps don't preserve order
+	// hence it should be tested separately
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t *testing.T, got []string, val string) {
+	t.Helper()
+	contains := false
+	for _, x := range got {
+		if x == val {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %s", got, val)
+	}
+}
