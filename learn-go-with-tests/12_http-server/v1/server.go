89c56742f7293a59812ace8bedc3684eb08bb144
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-11-15 10:23:39 +0530

    LrnGoTst: Add http-server unit test exercises
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ learn-go-with-tests/12_http-server/v1/server.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"net/http"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("20"))
+}
---
+++ learn-go-with-tests/12_http-server/v1/server_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	PlayerServer(response, request)
+
+	t.Run("return's Pepper's score", func(t *testing.T) {
+		got := response.Body.String()
+		want := "20"
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+}
