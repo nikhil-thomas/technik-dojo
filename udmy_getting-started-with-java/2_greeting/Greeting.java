056b240794e79944d8d3068f672f331a9e57b254
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-12-08 19:02:06 +0530

    Add a simple Java example
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ udmy_getting-started-with-java/2_greeting/Greeting.java
@@ -0,0 +1,9 @@
+class Greeting {
+    public static void main(String[] args) {
+        greet("Alex");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+}
