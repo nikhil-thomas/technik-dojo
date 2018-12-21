b4f3c1f764791e99b3d3698880baf49b81974555
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-12-21 08:45:34 +0530

    Shell Scripting: Add commented version of 01_hello-world.sh  Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ shell-scripting/01_sfronln_learn-linux-shell-scripting/c7/02_hello-world-improved.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##################################################
+# Author: Nikhil Thomas
+# Version: v1.0.0
+# Date: 2018-DEC-26
+# Description: prints Hello, World!
+# Usage: ./02_hello-world-improved.sh
+##################################################
+
+echo Hello, World!
+
