4e8be16c1c1eafc3c4856b630e0550ff0e036095
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-12-24 21:28:22 +0530

    Shell Scripting: Add printf exercise
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ shell-scripting/02_sfronln_linux-shell-scripting-cookbook-3rd-edition/c1/01_printf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##################################################
+# Author: Nikhil Thomas
+# Version: v1.0.0
+# Date: 24-DEC-2018
+# Description: A script to show the usage of printf
+# Usage: ./01_printf.sh
+##################################################
+
+printf "%-5s %-10s %-4s\n" No Name Mark
+printf "%-5s %-10s %-4.2f\n" 1 James 80.3456
+printf "%-5s %-10s %-4.2f\n" No Marry 90.9989
+printf "%-5s %-10s %-4.2f\n" No John 77.564
+
