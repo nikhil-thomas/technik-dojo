87b802badc38457cec897b8cfe62e6e771295de2
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-12-27 09:56:04 +0530

    Shell Scripting: Add an auto generate file script
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ shell-scripting/02_sfronln_linux-shell-scripting-cookbook-3rd-edition/c1/03_text-block-to-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##################################################
+# Author: Nikhil Thomas
+# Version: v1.0.0
+# Date: 27-DEC-2018
+# Description: A script to redirect from a text block inside a script
+# Usage: ./03_text-block-to-script.sh
+##################################################
+
+cat<<EOF>log.txt
+This is a generated file. Do not edit. Changes will be over written
+EOF
+
