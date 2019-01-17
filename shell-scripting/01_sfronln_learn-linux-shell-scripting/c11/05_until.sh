3d2181960c1cab6f3116ea3502f13676a6349715
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-01-17 19:56:24 +0530

    sscpr: Add script to loop using 'until'
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

--- shell-scripting/01_sfronln_learn-linux-shell-scripting/c11/05_until.sh
+++ shell-scripting/01_sfronln_learn-linux-shell-scripting/c11/05_until.sh
@@ -5,18 +5,18 @@
 # Version     : v1.0.0
 # Date        : 2019-JAN-17
 # Description : Script to print hello in a loop
-# Usage       : ./04_while.sh <name>
+# Usage       : ./05_until.sh <name>
 ##################################################
 
 if [ $# = 0 ]
 then
-  echo "usage ./04_while.sh <iteretions>"
+  echo "usage ./05_until.sh <iteretions>"
   exit 1
 fi
 
 itr=$1
 count=0
-while [[ ${count} -lt ${itr} ]]
+until [[ ${count} -gt ${itr} ]]
 do
   echo hello : ${count}
   sleep 0.5
