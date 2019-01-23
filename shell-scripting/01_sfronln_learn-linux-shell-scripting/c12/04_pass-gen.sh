e473215cbaa0e3dcfdef0b9657ff0aac674e2e5c
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-01-23 09:00:53 +0530

    sscpr: Add a script to generate a password
    Add a script to generate a password consisting of random characters
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ shell-scripting/01_sfronln_learn-linux-shell-scripting/c12/04_pass-gen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##################################################
+# Author      : Nikhil Thomas
+# Version     : v1.0.0
+# Date        : 2019-JAN-23
+# Description : Script to generate a password of given length
+#               with given length
+# Usage       : ./04_pass-gen.sh <length>
+##################################################
+
+if [[ $# -ne 1 ]]; then
+  echo "usage : ./04_pass-gen.sh <length>"
+  exit 1
+fi
+
+length=$1
+
+if [[ ! ${length} =~ ^[[:digit:]]+$ ]]; then
+  echo "invalid input : ${length}"
+  echo "usage : ./04_pass-gen.sh <length>"
+  exit 1
+fi
+
+password=$(tr -dc '[A-Za-z0-1]' < /dev/urandom | head -c ${length})
+printf "\npassword : %s\n" ${password}
+
